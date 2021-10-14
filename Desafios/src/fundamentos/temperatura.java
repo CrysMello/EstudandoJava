9d55c602a22ad9231bef827fd16de9f93ec25b42
CrysMello <crystianemello@hotmail.com>
2021-10-13 23:50:00 -0300

    desafio de conversão de temperatura

---
+++ Desafios/src/fundamentos/temperatura.java
@@ -0,0 +1,13 @@
+package fundamentos;
+// Desafio converter FAHRENHEIT PARA CELSIUS
+public class temperatura {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double fahrenheit = 74;
+		final double formula = 32;
+		double temperatura = (fahrenheit - formula)/1.8;
+		System.out.println(temperatura);
+	}
+
+}
